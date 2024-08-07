ccae31e98bb77b7e117d9269e309555762a8476c
Hamish Peebles <hamishpeebles@gmail.com>
2024-08-07 08:58:56 +0100

    Implement `Display` for Price and Amount + consolidate code (#12)

--- src/types/src/lib.rs
+++ src/types/src/lib.rs
@@ -1,5 +1,6 @@
 use async_trait::async_trait;
 use std::collections::BTreeMap;
+use std::fmt::{Display, Formatter, Write};
 use std::str::FromStr;
 use std::sync::Arc;
 use tokio::sync::broadcast::{Receiver, Sender};
@@ -110,11 +111,7 @@ impl FromStr for Price4Decimals {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (whole, fractional) = s.split_once('.').unwrap_or((s, ""));
-        let fractional_padded = format!("{fractional:0<4}");
-
-        let units =
-            (1_0000 * u128::from_str(whole).unwrap()) + u128::from_str(&fractional_padded).unwrap();
+        let units = parse_units_from_decimal(s, 4)?;
 
         Ok(Price4Decimals { units })
     }
@@ -124,14 +121,54 @@ impl FromStr for Amount8Decimals {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (whole, fractional) = s.split_once('.').unwrap_or((s, ""));
-        let fractional_padded = format!("{fractional:0<8}");
-
-        let units = (1_0000_0000 * u128::from_str(whole).unwrap())
-            + u128::from_str(&fractional_padded).unwrap();
+        let units = parse_units_from_decimal(s, 8)?;
 
         Ok(Amount8Decimals { units })
     }
+}
+
+impl Display for Price4Decimals {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        format_units_to_decimal(self.units, 4, f)
+    }
+}
+
+impl Display for Amount8Decimals {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        format_units_to_decimal(self.units, 8, f)
+    }
+}
+
+fn parse_units_from_decimal(s: &str, decimals: usize) -> Result<u128, ()> {
+    let (whole, fractional) = s.split_once('.').unwrap_or((s, ""));
+    let fractional_padded = format!("{fractional:0<decimals$}");
+    let units_per_whole = 10u128.pow(decimals as u32);
+
+    let units = (units_per_whole * u128::from_str(whole).unwrap())
+        + u128::from_str(&fractional_padded).unwrap();
+
+    Ok(units)
+}
+
+fn format_units_to_decimal(
+    units: u128,
+    decimals: usize,
+    f: &mut Formatter<'_>,
+) -> std::fmt::Result {
+    let units_per_whole = 10u128.pow(decimals as u32);
+    let fractional = units % units_per_whole;
+    let whole = units / units_per_whole;
+
+    Display::fmt(&whole, f)?;
+
+    if fractional != 0 {
+        f.write_char('.')?;
+        let fractional_padded = format!("{fractional:0<decimals$}");
+        let fractional_trimmed = fractional_padded.trim_end_matches('0');
+        f.write_str(fractional_trimmed)?;
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -143,17 +180,23 @@ mod tests {
     #[test_case("1.234", 12340)]
     #[test_case("123.4567", 1234567)]
     #[test_case("1234", 12340000)]
-    fn parse_price(s: &str, expected_units: u128) {
+    fn price_roundtrip(s: &str, expected_units: u128) {
         let result = Price4Decimals::from_str(s).unwrap();
         assert_eq!(result.units, expected_units);
+
+        let fmt = result.to_string();
+        assert_eq!(fmt.as_str(), s);
     }
 
     #[test_case("0.1234", 12340000)]
     #[test_case("1.234", 123400000)]
     #[test_case("123.45678901", 12345678901)]
     #[test_case("1234", 123400000000)]
-    fn parse_amount(s: &str, expected_units: u128) {
+    fn amount_roundtrip(s: &str, expected_units: u128) {
         let result = Amount8Decimals::from_str(s).unwrap();
         assert_eq!(result.units, expected_units);
+
+        let fmt = result.to_string();
+        assert_eq!(fmt.as_str(), s);
     }
 }
